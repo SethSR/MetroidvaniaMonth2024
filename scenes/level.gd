a02fe93f9ef0b96c519ca83c10642f40a8d0d50c
Seth Renshaw <srenshaw1@gmail.com>
2024-03-02 23:15:34 -0600

    - WARNING: There may be a potentially annoying issue with resource reference-loops. Godot didn't like it when I tried opening test_map_left because it had a reference to test_map_right, which had a reference back to left. If you run into this open the .tscn file in a text editor and just delete the resource reference at the top of the file, and the "NextLevel" line under the DoorLink node. If this keeps happening, we may have to find _another_ way of dealing with doors & level-loading. - My previous changes got wiped when `git mv`-ing turret_controller.gd to level.gd, so I fixed that. - Moved the Player back to the "start" of test_map_left.

--- scenes/level.gd
+++ scenes/level.gd
@@ -1,8 +1,15 @@
-extends Node
+extends Node2D
 
-class_name TurretController
+class_name Level
 
 const BULLET_SCENE: PackedScene = preload("res://scenes/bullet.tscn")
+
+var doors: Array[Door] = []
+
+func setup() -> void:
+	for child: Node in get_children():
+		if child is Door:
+			doors.push_back(child as Door)
 
 func _on_turret_shoot(lifetime: float, direction: Vector2, location: Vector2, speed: float) -> void:
 	var bullet: Bullet = BULLET_SCENE.instantiate()
