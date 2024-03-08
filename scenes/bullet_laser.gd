44f59a861930270208855d49d77cf7e9b56de337
Seth Renshaw <srenshaw1@gmail.com>
2024-03-08 04:42:52 -0600

    - Laser are in! cooldowns are a bit weird, but that's normal at this point. - For cooldowns x is the firing time, and y is the rest time. - Delay is an extra rest time at spawn. - For a constant beam, set any cooldown.x to less than 0, and the fire/rest cycle will stop at that point. - For sound-related reasons, rest times (y) must be greater or equal to 0.25.

---
+++ scenes/bullet_laser.gd
@@ -0,0 +1,45 @@
+extends StaticBody2D
+
+class_name BulletLaser
+
+@onready var sprite: Sprite2D = $Sprite2D
+@onready var timer: Timer = $Timer
+
+enum State {
+	Activating,
+	Active,
+	Inactive,
+}
+
+const LASER_SMALL: Rect2i = Rect2i( 80, 256, 16, 16)
+const LASER_LARGE: Rect2i = Rect2i(128, 144, 16, 16)
+
+var state: State = State.Activating
+var saved_pos: Vector2 = Vector2.ZERO
+var polarity: Enums.Polarity = Enums.Polarity.RED
+
+func _ready() -> void:
+	saved_pos = sprite.position
+	match polarity:
+		Enums.Polarity.RED:  modulate = Color.RED
+		Enums.Polarity.BLUE: modulate = Color.BLUE
+
+func _on_timer_timeout() -> void:
+	match state:
+		State.Activating:
+			sprite.region_rect = LASER_SMALL
+			sprite.position = Vector2.ZERO
+			state = State.Active
+		State.Active:
+			sprite.region_rect = LASER_LARGE
+			sprite.position.x = saved_pos.x + randf_range(-0.5, 0.5)
+			sprite.position.y = saved_pos.y + randf_range(-0.5, 0.5)
+			sprite.flip_h = randi_range(0, 1) == 0
+		State.Inactive:
+			queue_free()
+
+func deactivate() -> void:
+	state = State.Inactive
+	sprite.region_rect = LASER_SMALL
+	sprite.position = Vector2.ZERO
+	timer.start()
